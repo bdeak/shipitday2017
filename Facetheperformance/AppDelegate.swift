63bfd31d548ff8eed68d76daf375d8df3b24b7fa
Dominik Kratky <dominik@Dominiks-MacBook-Pro.local>
2017-12-08 00:09:16 +0100

    Add Class WebClass

--- Facetheperformance/AppDelegate.swift
+++ Facetheperformance/AppDelegate.swift
@@ -12,7 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let web = WebClass()
+   
     
     let displayStatusChanged: CFNotificationCallback = { center, observer, name, object, info in
         let str = name!.rawValue as CFString
@@ -22,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             isDisplayStatusLocked.synchronize()
         }
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let isDisplayStatusLocked = UserDefaults.standard
@@ -42,12 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         
         let isDisplayStatusLocked = UserDefaults.standard
@@ -79,14 +80,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 }
                 _ = URLRequest(url: url)
                 print("Lock API call sent")
+                web.lockAction()
             }
         }
         
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "updateExitStatus"), object: nil)
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         print("Back to foreground.")
+        web.unlockAction()
         
         //restore lock screen setting
         
@@ -96,15 +99,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
-
