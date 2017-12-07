6674ec8def013b1c57d2d7f53617ab50ffd12f72
Dominik Kratky <dominik@Dominiks-MacBook-Pro.local>
2017-12-07 13:40:35 +0100

    Initial Commit

---
+++ Facetheperformance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facetheperformance
+//
+//  Created by Dominik Kratky on 07.12.17.
+//  Copyright © 2017 Dominik Kratky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
