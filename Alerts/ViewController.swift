e13efbc310e1a57144c0a2f0c0e704df97eb8412
Jana Bales <bales.jana@gmail.com>
2017-12-08 11:14:11 +0100

    added lock/unlock authorization via face and touch ID

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController, UIAlertViewDelegate {
 
@@ -35,13 +36,56 @@ class ViewController: UIViewController, UIAlertViewDelegate {
     public func alertView(_ alertView: UIAlertView, clickedButtonAt buttonIndex: Int) {
         let web = WebClass()
         print(buttonIndex)
+        let myContext = LAContext()
+        let myLocalizedReasonString = "Authorization required for lock/unlock process."
+        
+        var authError: NSError?
         if alertView.tag == 1 {
             if buttonIndex == 0 {
-                web.unlockAction()
+                
+                if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                    myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                    
+                        if success {
+                                web.unlockAction()
+                            } else {
+                                self.presentAlertController(withTitle: "Error", message: "Authentication failed")
+                                // User did not authenticate successfully, look at error and take appropriate action
+                            }
+                        }
+                    } else {
+                        // Could not evaluate policy; look at authError and present an appropriate message to user
+                        self.presentAlertController(withTitle: "Error", message: "Authentication failed or no Face and Touch ID available")
+                    }
+                
             } else {
-                web.lockAction()
+                if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                    myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                        
+                        if success {
+                            web.lockAction()
+                        } else {
+                            self.presentAlertController(withTitle: "Error", message: "Authentication failed")
+                            // User did not authenticate successfully, look at error and take appropriate action
+                        }
+                    }
+                } else {
+                    // Could not evaluate policy; look at authError and present an appropriate message to user
+                    self.presentAlertController(withTitle: "Error", message: "Authentication failed or no Face and Touch ID available")
+                }
             }
         }
+    }
+    
+        
+    func presentAlertController(withTitle title: String?, message: String?) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok",
+                                                style: .default,
+                                                handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
     
 }
